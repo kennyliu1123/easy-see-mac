d040992c4aec79c7e0f92f9b22fb6936e01c1a13
kennyliu1123 <liuxiaokaisss@hotmail.com>
2025-05-26 04:14:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
+import subprocess
 import os
 import sys
-import streamlit.web.bootstrap
 
 def resource_path(filename):
-    # 如果使用 PyInstaller 打包，_MEIPASS 是临时目录
-    base = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
-    return os.path.join(base, filename)
+    if hasattr(sys, '_MEIPASS'):
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, filename)
 
 if __name__ == "__main__":
     app_file = resource_path("app.py")
-    streamlit.web.bootstrap.run(
-        app_file,
-        f"run.py {app_file}",
-        [],
-        flag_options={}
-    )
+    subprocess.run([sys.executable, "-m", "streamlit", "run", app_file])
