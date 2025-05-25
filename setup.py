bce94301fa2c68177b51bbfc69c88bb6a5553f3e
kennyliu1123 <liuxiaokaisss@hotmail.com>
2025-05-26 00:08:38 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup
 
 APP = ['main.py']
-DATA_FILES = [('resources', ['app.py', 'state_init.py'])]  # 指定资源文件目录
+DATA_FILES = ['app.py', 'state_init.py']
 OPTIONS = {
     'argv_emulation': True,
     'includes': ['streamlit', 'pandas', 'chardet'],
     'packages': ['streamlit', 'pandas', 'chardet'],
+    'resources': DATA_FILES,  # 指定需打包进去的文件
 }
 
 setup(
     app=APP,
-    data_files=DATA_FILES,
+    data_files=[],
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
