d16b30ac2ff8cb952d0a7e108ca8f23599d6759b
kennyliu1123 <liuxiaokaisss@hotmail.com>
2025-05-26 05:37:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import os
+import subprocess
 import sys
-import streamlit.web.bootstrap
 
 def resource_path(filename):
-    if hasattr(sys, '_MEIPASS'):
-        return os.path.join(sys._MEIPASS, filename)
-    return os.path.join(os.path.abspath("."), filename)
+    base = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(base, filename)
 
 if __name__ == "__main__":
     app_file = resource_path("app.py")
-    streamlit.web.bootstrap.run(
-        app_file,
-        f"streamlit run {app_file}",
-        [],
-        {}  # flag_options，如有需要可加 e.g. {"server.headless": True}
-    )
+    subprocess.run([
+        sys.executable,
+        "-m", "streamlit", "run", app_file,
+        "--server.headless=true",
+        "--server.port=3000",
+        "--browser.serverAddress=localhost"
+    ])
