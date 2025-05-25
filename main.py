2d88b52300426ebb664997fc5329955ec9a80296
kennyliu1123 <liuxiaokaisss@hotmail.com>
2025-05-26 00:07:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import subprocess
 import os
+import subprocess
 import sys
+import pathlib
 
 def resource_path(filename):
-    base = os.path.abspath(os.path.dirname(__file__))
-    return os.path.join(base, "resources", filename)
+    base = getattr(sys, '_MEIPASS', os.path.abspath(os.path.dirname(__file__)))
+    return os.path.join(base, filename)
 
 if __name__ == "__main__":
     app_file = resource_path("app.py")
-    subprocess.Popen(["/usr/local/bin/python3", "-m", "streamlit", "run", app_file])
+    streamlit_path = resource_path("venv/bin/streamlit")
+    subprocess.Popen([streamlit_path, "run", app_file])
