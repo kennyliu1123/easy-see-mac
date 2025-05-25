734c1dbe2155966b9d18a4c1634bcf797bea0a79
kennyliu1123 <liuxiaokaisss@hotmail.com>
2025-05-25 22:38:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import subprocess
 import os
-import streamlit.web.bootstrap
+import sys
 
 def resource_path(filename):
-    base_path = os.path.abspath(os.path.dirname(__file__))
-    return os.path.join(base_path, filename)
+    base = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(base, "resources", filename)
 
 if __name__ == "__main__":
-    app_path = resource_path("app.py")
-    streamlit.web.bootstrap.run(app_path, f"streamlit run {app_path}", [])
+    app_file = resource_path("app.py")
+    subprocess.Popen(["/usr/local/bin/python3", "-m", "streamlit", "run", app_file])
